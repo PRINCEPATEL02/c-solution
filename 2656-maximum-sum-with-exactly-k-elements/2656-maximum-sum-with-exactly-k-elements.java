11e14d58f8672316fa99bda1732afdc7e83c93c6
Prince <157803333+PRINCEPATEL02@users.noreply.github.com>
2026-04-25 11:16:45 +0530

    Time: 1 ms (100%), Space: 47 MB (65.81%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maximizeSum(int[] nums, int k) {
-        Arrays.sort(nums);
-        int ans = 0;
-        int a = nums[nums.length - 1];
-        while(k!=0){
-            ans += a;
-            k--;
-            a += 1;
+        int max = 0;
+
+        for (int num : nums) {
+            if (num > max) {
+                max = num;
+            }
         }
-        return ans;
+
+        return (k * (2 * max + (k - 1))) / 2;
     }
 }
