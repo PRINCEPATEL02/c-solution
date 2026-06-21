eecd35ade8270f45ab9aa29fac5b01ce1f8d7421
Prince <157803333+PRINCEPATEL02@users.noreply.github.com>
2026-06-21 08:49:55 +0530

    Time: 1 ms (99.97%), Space: 47 MB (27.48%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num , map.getOrDefault(num , 0) + 1);
+        int ans = 0;
+        for(int i : nums){
+            ans ^= i;
         }
-        for(int key : map.keySet()){
-            if(map.get(key) == 1){
-                return key;
-            }
-        }
-        return -1;
+        return ans;
     }
 }
