5d5981156f1a97af39180c15d7e4acf5bbe52679
Prince <157803333+PRINCEPATEL02@users.noreply.github.com>
2026-06-24 10:53:07 +0530

    Time: 9 ms (77.92%), Space: 47.3 MB (42.86%) - LeetHub

---
+++ 0828-count-unique-characters-of-all-substrings-of-a-given-string/0828-count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int uniqueLetterString(String s) {
+
+        int n = s.length();
+        int[][] pos = new int[26][2];
+
+        for (int i = 0; i < 26; i++) {
+            pos[i][0] = -1;
+            pos[i][1] = -1;
+        }
+
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int c = s.charAt(i) - 'A';
+
+            ans += (long)(i - pos[c][1]) * (pos[c][1] - pos[c][0]);
+
+            pos[c][0] = pos[c][1];
+            pos[c][1] = i;
+        }
+
+        for (int c = 0; c < 26; c++) {
+            ans += (long)(n - pos[c][1]) *
+                   (pos[c][1] - pos[c][0]);
+        }
+
+        return (int)ans;
+    }
+}
